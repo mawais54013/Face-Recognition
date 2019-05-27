20d8793767fe62a71cca1f6b97fab103a327945a
MAwais <mawais54013@outlook.com>
2019-05-27 12:06:01 -0700

    added postgres

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt =  require('bcrypt-nodejs');
-const cors = require('cors')
+const cors = require('cors');
+const knex = require('knex')
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'muhammadawais',
+        password : '',
+        database : 'smart-brain'
+    }
+});
+
+console.log(postgres.select('*').from('users'));
 
 const app = express();
 
