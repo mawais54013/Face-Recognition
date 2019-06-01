1b9f372b2a5867983c382d49b9447d30c61074c3
MAwais <mawais54013@outlook.com>
2019-06-01 09:03:15 -0700

    fixed styles

--- front-end/src/components/Register/Register.js
+++ front-end/src/components/Register/Register.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const textStyle = {
+  fontSize: '20px'
+}
+
 class Register extends React.Component {
   constructor(props) {
     super(props);
@@ -50,20 +54,20 @@ class Register extends React.Component {
               <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                 <legend className="f1 fw6 ph0 mh0">Register</legend>
                 <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                  <label className="db fw6 lh-copy f6" htmlFor="name" style={textStyle}>Name</label>
                   <input onChange={this.onNameChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
                 </div>
                 <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                  <label className="db fw6 lh-copy f6" htmlFor="email-address" style={textStyle}>Email</label>
                   <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                 </div>
                 <div className="mv3">
-                  <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                  <label className="db fw6 lh-copy f6" htmlFor="password" style={textStyle}>Password</label>
                   <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                 </div>
               </fieldset>
               <div className="">
-                <input onClick= {this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
+                <input onClick= {this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" style={textStyle}/>
               </div>
             </div>
         </main>
--- front-end/src/components/Signin/SignIn.js
+++ front-end/src/components/Signin/SignIn.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+const textStyle = {
+  fontSize: '20px'
+}
+
+const boldText = {
+  fontWeight: 'bold',
+  fontSize: '20px'
+}
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -44,19 +53,19 @@ class SignIn extends React.Component {
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <label className="db fw6 lh-copy f6" htmlFor="email-address" style={textStyle}>Email</label>
               <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <label className="db fw6 lh-copy f6" htmlFor="password" style={textStyle}>Password</label>
               <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
             </div>
           </fieldset>
           <div className="">
-            <input onClick= {this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+            <input onClick= {this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" style={textStyle}/>
           </div>
           <div className="lh-copy mt3">
-            <p onClick= {() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+            <p onClick= {() => onRouteChange('register')} className="f6 link dim black db pointer" style={boldText}>Register</p>
           </div>
         </div>
       </main>
