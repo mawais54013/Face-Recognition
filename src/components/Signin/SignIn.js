1e435a74ac81816e43f8a073b8d1205cd0f7f206
MAwais <mawais54013@outlook.com>
2019-06-01 09:53:53 -0700

    fixed front end issue

--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -27,7 +27,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://hidden-temple-22666.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
