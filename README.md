00706b381f6d70b5a749aaeafd77c27a18d4dacb
MAwais <mawais54013@outlook.com>
2019-06-03 20:57:32 -0700

    readme and fix code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Face - Recognition
 
+The application is a image face recognition app using Postgres, React, Clarifai, and Tachyons. Users can login or register to the app and paste a link to a image in the input area that will detect every face in the image with a box while updating the user's count with every image. 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,10 +9,13 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
         );
     } else {
         return (
+            <div>
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
                 <p onClick = {() => onRouteChange('signin')} className='b f3 link dim white underline pa3 pointer'>Sign In</p>
                 <p onClick = {() => onRouteChange('register')} className='b f3 link dim white underline pa3 pointer'>Register</p>
             </nav>
+            <h1>Welcome To Face-Recognition</h1>
+            </div>
         );
     }
 }
